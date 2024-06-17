bb4e57d73a37dfd047b4859734534f9c5960098a
hiroyama-icolle <134476471+hiroyama-icolle@users.noreply.github.com>
2024-06-17 15:52:24 +0900

    課題を提出していますよ

---
+++ def4_kadai.py
@@ -0,0 +1,20 @@
+# s24000 def4_kadai.py
+# def4.pyをアレンジして
+# メインの処理をmain()関数に行わせるおみくじプログラム
+#
+
+import random
+
+# ランダムでkujiの中の一つを返す関数
+def omikuji():
+    kuji = ["大吉","中吉","小吉","凶"]
+    return random.choice(kuji)
+
+# エントリーポイントの定義
+def main():
+    kekka = omikuji()
+    print("結果は", kekka, "です")
+
+if __name__ == "__main__":
+    main()
+
